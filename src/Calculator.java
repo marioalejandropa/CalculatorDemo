cfd04b4de838e1170b62c9e1077b4fad861e5227
Alejandro Perez <mario.perez@globant.com>
2025-01-21 13:49:27 -0500

    added suma and resta methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Calculator {
+
+    public static int sumar(int a, int b){
+        int result;
+        result = a + b;
+        return result;
+    }
+
+    public static int restar(int a, int b){
+        int result;
+        result = a - b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int suma = sumar(5, 7);
+        int resta = restar(10, 5);
+        System.out.print("suma = " + suma + " "+ "resta = " + resta);
+    }
 }
